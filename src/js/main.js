52a06efd71a07503bef7ea5b4675e73038a17e2d
Stephen Chen <syhchen2012@gmail.com>
2014-11-11 15:17:05 -0800

    Finished prototype.

--- src/js/main.js
+++
@@ -1,5 +0,0 @@
-// var front = document.getElementById('video-00-00'), back_content = "&lt;h1>I'm the back!&lt;/h1>", back;
-//
-// // when the correct action happens, call flip!
-// back = flippant.flip(front, back_content);
-// // this creates the back element, sizes it and flips it around.
